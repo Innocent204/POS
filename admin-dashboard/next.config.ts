dbac94cf833a1ef1d8ec4647137183d3f0669903
Developer <developer@example.com>
2026-05-14 12:48:58 +0200

    next.config fix

--- admin-dashboard/next.config.ts
+++ admin-dashboard/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const API_URL = process.env.API_URL || 'http://165.227.119.71/api/v1';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: undefined,
-  },
   images: {
     localPatterns: [
       {
